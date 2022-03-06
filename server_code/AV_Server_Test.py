25a801a170ae288912cf3a01156b9407289c8463
devavinashmaurya@gmail.com <devavinashmaurya@gmail.com>
2022-03-06 00:42:34 +0000

    Edited AV_Server_Test

--- server_code/AV_Server_Test.py
+++ server_code/AV_Server_Test.py
@@ -1,5 +1,5 @@
 import anvil.server
-from google.cloud import storage
+# from google.cloud import storage
 
 # @anvil.server.http_endpoint("/get_product/:id")
 @anvil.server.callable
@@ -11,7 +11,7 @@ def get_product(id):
 @anvil.server.callable
 def send_audio_file(file):
 #   ip = anvil.server.request.remote_address
-  upload_blob()
+#   upload_blob()
   res = {
     "name": file.name,
     "length": file.length,
@@ -20,11 +20,11 @@ def send_audio_file(file):
   return res
 
 
-def upload_blob(source_file_name):
-  bucket_name = "make3400_cloudbuild"
-  source_file_name = local/path/to/file
-  destination_blob_name = "gs://make3400_cloudbuild"
-  storage_client = storage.Client()
-  bucket = storage_client.bucket(bucket_name)
-  blob = bucket.blob(destination_blob_name)
-  blob.upload_from_filename(source_file_name)
+# def upload_blob(source_file_name):
+#   bucket_name = "make3400_cloudbuild"
+#   source_file_name = local/path/to/file
+#   destination_blob_name = "gs://make3400_cloudbuild"
+#   storage_client = storage.Client()
+#   bucket = storage_client.bucket(bucket_name)
+#   blob = bucket.blob(destination_blob_name)
+#   blob.upload_from_filename(source_file_name)
