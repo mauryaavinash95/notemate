053a783bf36db8a2b823039b5d9225bcfd95c925
devavinashmaurya@gmail.com <devavinashmaurya@gmail.com>
2022-03-06 01:09:47 +0000

    Edited settings and AV_Server_Test

--- server_code/AV_Server_Test.py
+++ server_code/AV_Server_Test.py
@@ -14,21 +14,9 @@ def send_audio_file(file):
 #   upload_blob()
   headers = {
     'Authorization': 'Bearer ya29.A0ARrdaM93tW9QhwnJbE-7G_arWDI3g6WE58jnEYXnjx3P7QoXxIKiPL24pQDSZLp_PPQlWiJLAXs32h0-W8w3jtVf8Wk4WL4Y7yc9650dDxzqK4KXcFhs4gHirlK3Ih9v6O9noQJ0h-5WhzaQK5a1luDD4YPh',
-    'Content-Type': 'image/png',
+    'Content-Type': file.content_type,
   }
-  params = (
-      ('uploadType', 'media'),
-      ('name', 'test.png'),
-  )
-  response = anvil.http.request('https://storage.googleapis.com/upload/storage/v1/b/make3400_cloudbuild/o', method="POST", headers=headers, params=params, data=file)
-#   resp = anvil.http.request(url="https://storage.googleapis.com/upload/storage/v1/b/make3400_cloudbuild/o?uploadType=media&name=test1.png",
-#                     method="POST",
-#                     data=file,
-#                     headers= {
-#                       "Authentication": "ya29.A0ARrdaM93tW9QhwnJbE-7G_arWDI3g6WE58jnEYXnjx3P7QoXxIKiPL24pQDSZLp_PPQlWiJLAXs32h0-W8w3jtVf8Wk4WL4Y7yc9650dDxzqK4KXcFhs4gHirlK3Ih9v6O9noQJ0h-5WhzaQK5a1luDD4YPh",
-#                       "Content-Type": "image/png"
-#                     })
-  print(f"Response MIME type: {resp.content_type}")
+  resp = anvil.http.request(f'https://storage.googleapis.com/upload/storage/v1/b/make3400_cloudbuild/o?uploadType=media&name={file.name}', method="POST", headers=headers, data=file)
   res = {
     "name": file.name,
     "length": file.length,
