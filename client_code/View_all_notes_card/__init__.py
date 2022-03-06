241d5c7e39e4bbe3ddcb5824791c3941edcfa8c8
devavinashmaurya@gmail.com <devavinashmaurya@gmail.com>
2022-03-06 02:00:43 +0000

    Edited View_all_notes_card

--- client_code/View_all_notes_card/__init__.py
+++ client_code/View_all_notes_card/__init__.py
@@ -4,6 +4,7 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ..View_notes_card import View_notes_card
 
 class View_all_notes_card(View_all_notes_cardTemplate):
   def __init__(self, **properties):
@@ -12,6 +13,6 @@ class View_all_notes_card(View_all_notes_cardTemplate):
 #     self.repeating_panel_1.items = app_tables.notes
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.repeating_panel_1.item_template = View_notes_card()
+    self.repeating_panel_1.item_template = View_notes_card
     self.repeating_panel_1.items = app_tables.notes.search()
     # Any code you write here will run when the form opens.
