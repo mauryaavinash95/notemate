83ae9666486144492c78de84e274262d26d94f07
devavinashmaurya@gmail.com <devavinashmaurya@gmail.com>
2022-03-06 03:55:47 +0000

    Created temp and temp.ItemTemplate3; Edited settings, View_all_notes_card, View_all_notes_card.ItemTemplate2 and Single_Note_View

--- client_code/View_all_notes_card/__init__.py
+++ client_code/View_all_notes_card/__init__.py
@@ -12,7 +12,6 @@ class View_all_notes_card(View_all_notes_cardTemplate):
     self.init_components(**properties)
 #     self.repeating_panel_1.items = app_tables.notes
     # Set Form properties and Data Bindings.
-    self.init_components(**properties)
 #     self.repeating_panel_1.item_template = Single_Note_View
     self.repeating_panel_1.items = app_tables.notes.search()
     # Any code you write here will run when the form opens.
---
+++ client_code/temp/__init__.py
@@ -0,0 +1,13 @@
+from ._anvil_designer import tempTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class temp(tempTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.repeating_panel_1.items = app_tables.test.search()
+    # Any code you write here will run when the form opens.
