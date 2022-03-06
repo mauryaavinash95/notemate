0ec57fbb0657f0ff9ee5e58d268e78486528eb70
devavinashmaurya@gmail.com <devavinashmaurya@gmail.com>
2022-03-06 10:29:03 +0000

    Edited Main, All_topics.ItemTemplate3, settings, View_all_notes_card, Upload_And_Process, View_all_notes_card.ItemTemplate2, Upload_And_Process.ItemTemplate1, View_all_notes_card.Single_Note_View, Server_Test, Audio_Record and All_topics

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import MainTemplate
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
@@ -10,8 +13,13 @@ from ..All_topics import All_topics
 class Main(MainTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
-    self.init_components(**properties)    
-
+    self.init_components(**properties)
+    while anvil.users.get_user() is None:
+      try:
+        user = anvil.users.login_with_google()
+      except anvil.users.AuthenticationFailed as e:
+        alert("Your username/password is wrong. Please retry!")
+    
   def button_2_click(self, **event_args):
     self.content_panel.clear()
     self.content_panel.add_component(Upload_And_Process(), full_width_row=True)
@@ -23,6 +31,12 @@ class Main(MainTemplate):
   def button_3_click(self, **event_args):
     self.content_panel.clear()
     self.content_panel.add_component(All_topics(), full_width_row=True)
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.users.logout()
+    open_form()
+
 
 
 
