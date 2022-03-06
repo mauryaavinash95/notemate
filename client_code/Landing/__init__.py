7c8072561260dca5c6cd2766d02fceeaf8dc4049
devavinashmaurya@gmail.com <devavinashmaurya@gmail.com>
2022-03-06 13:11:01 +0000

    Created Landing; Edited Main; Deleted Form1

--- client_code/Landing/__init__.py
+++ client_code/Landing/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import LandingTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -8,9 +8,19 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Form1(Form1Template):
+class Landing(LandingTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    while anvil.users.get_user() is None:
+      try:
+        user = anvil.users.login_with_google()
+      except anvil.users.AuthenticationFailed as e:
+        alert("Your username/password is wrong. Please retry!")
+
+
--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -9,16 +9,15 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 from ..Upload_And_Process import Upload_And_Process
 from ..All_topics import All_topics
+from ..Landing import Landing
 
 class Main(MainTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    while anvil.users.get_user() is None:
-      try:
-        user = anvil.users.login_with_google()
-      except anvil.users.AuthenticationFailed as e:
-        alert("Your username/password is wrong. Please retry!")
+    self.content_panel.clear()
+    self.content_panel.add_component(Landing(), full_width_row=True)
+    
     
   def button_2_click(self, **event_args):
     self.content_panel.clear()
