33be767f06abdd6f194433ddc83e15070deb0627
devavinashmaurya@gmail.com <devavinashmaurya@gmail.com>
2022-03-06 04:03:12 +0000

    Edited temp and settings

--- client_code/temp/__init__.py
+++ client_code/temp/__init__.py
@@ -9,5 +9,6 @@ class temp(tempTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+#     self.repeating_panel_1.items = [{"Name": "a", "ID": "abc"}]
     self.repeating_panel_1.items = app_tables.test.search()
     # Any code you write here will run when the form opens.
