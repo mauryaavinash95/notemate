5621b62b328928b7552b264af85a6c5b421363df
devavinashmaurya@gmail.com <devavinashmaurya@gmail.com>
2022-03-06 01:29:33 +0000

    Edited AV_Server_Test

--- server_code/AV_Server_Test.py
+++ server_code/AV_Server_Test.py
@@ -1,7 +1,5 @@
 import anvil.server
-# from google.cloud import storage
 
-# @anvil.server.http_endpoint("/get_product/:id")
 @anvil.server.callable
 def get_product(id):
 #   ip = anvil.server.request.remote_address
@@ -10,8 +8,6 @@ def get_product(id):
 
 @anvil.server.callable
 def send_audio_file(file):
-#   ip = anvil.server.request.remote_address
-#   upload_blob()
   headers = {
     'Authorization': 'Bearer ya29.A0ARrdaM93tW9QhwnJbE-7G_arWDI3g6WE58jnEYXnjx3P7QoXxIKiPL24pQDSZLp_PPQlWiJLAXs32h0-W8w3jtVf8Wk4WL4Y7yc9650dDxzqK4KXcFhs4gHirlK3Ih9v6O9noQJ0h-5WhzaQK5a1luDD4YPh',
     'Content-Type': file.content_type,
