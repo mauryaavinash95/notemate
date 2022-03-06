6cd6ad332fccaac0a07657f1143291ae79b6de09
devavinashmaurya@gmail.com <devavinashmaurya@gmail.com>
2022-03-06 09:31:28 +0000

    Edited Upload_And_Process, Server_Test and Audio_Record

--- client_code/Upload_And_Process/__init__.py
+++ client_code/Upload_And_Process/__init__.py
@@ -4,33 +4,26 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from datetime import datetime
 
 class Upload_And_Process(Upload_And_ProcessTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
-    # Any code you write here will run when the form opens.
-
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
+    self.text_box_2.text = "Lecture-"+datetime.now().strftime("%m-%d-%y-%H:%M")
+    self.text_box_1.text = "Topic:Data_Structures"
     
-    pass
-
-  def file_loader_2_change(self, file, **event_args):
-    """This method is called when a new file is loaded into this FileLoader"""
-    pass
-
   def file_loader_1_change(self, file, **event_args):
     """This method is called when a new file is loaded into this FileLoader"""
     res = anvil.server.call('send_audio_file', file=file)
     print("Response is", res)
     n = Notification(f"Upload successful for file: {file.name}, of size (B): {file.length}", timeout=5)
     n.show()
-#     print(f"The file's name is: {file.name}")
-#     print(f"The number of bytes in the file is: {file.length}")
-#     print(f"The file's content type is: {file.content_type}")
-    
+
+
+
+
+  
 
 
 
--- server_code/Server_Test.py
+++ server_code/Server_Test.py
@@ -9,10 +9,10 @@ def get_product(id):
 @anvil.server.callable
 def send_audio_file(file):
   headers = {
-    'Authorization': 'Bearer ya29.A0ARrdaM93tW9QhwnJbE-7G_arWDI3g6WE58jnEYXnjx3P7QoXxIKiPL24pQDSZLp_PPQlWiJLAXs32h0-W8w3jtVf8Wk4WL4Y7yc9650dDxzqK4KXcFhs4gHirlK3Ih9v6O9noQJ0h-5WhzaQK5a1luDD4YPh',
+    'Authorization': 'Bearer ya29.A0ARrdaM8bXb8tguHAoIgYvCiRyXETurDnh8kNF-tYOagBvO1KLnm6arORWxSqZ_N1O_UIEvmPzmw88HOX8BTn0tIMtxoPd-z1vCfpOgSHYDsjUAK0ojRVnJDFP6qgmnwxjKxK16nv1-jzLP7IjGFVaJPgicSs',
     'Content-Type': file.content_type,
   }
-  resp = anvil.http.request(f'https://storage.googleapis.com/upload/storage/v1/b/make3400_cloudbuild/o?uploadType=media&name={file.name}', method="POST", headers=headers, data=file)
+  resp = anvil.http.request(f'https://storage.googleapis.com/upload/storage/v1/b/make3400_audiofiles/o?uploadType=media&name={file.name}', method="POST", headers=headers, data=file)
   res = {
     "name": file.name,
     "length": file.length,
@@ -23,7 +23,7 @@ def send_audio_file(file):
 @anvil.server.http_endpoint("/transcript/:id")
 def insert_transcript(id):
   ip = anvil.server.request.remote_address
-  data = anvil.server.request.body
+  data = anvil.server.request.body_json["response"]
   return {
     "status": "Okie-dokey",
     "your_ip": ip,
@@ -34,7 +34,7 @@ def insert_transcript(id):
 @anvil.server.http_endpoint("/summary/:id")
 def insert_summary(id):
   ip = anvil.server.request.remote_address
-  data = anvil.server.request.body
+  data = anvil.server.request.body_json["response"]
   return {
     "status": "Okie-dokey",
     "your_ip": ip,
